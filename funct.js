e0b18bceff1698b08c634fde68198b7300ed0ac8
paxci <diego_g95@utp.edu.co>
2021-06-14 18:34:28 -0500

    se termina el proyecto con algunas funcionalidades pero incompleto

---
+++ funct.js
@@ -0,0 +1,58 @@
+
+//Principal Slider
+
+$(document).ready(function(){
+    $('.principal-slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        autoplay: false
+      });
+  },
+
+//
+
+  $('.shelf-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  })
+  );
+
+  //Hamburguer responsive menu
+
+  document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
